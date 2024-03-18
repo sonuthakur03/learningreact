97d7a84981ea3162c409e8e972d020f9829cb897
Sonu Thakur <rsonuth789@gmail.com>
2024-03-18 19:33:29 +0545

    managed variables name

--- 03_tailwindprops/src/App.jsx
+++ 03_tailwindprops/src/App.jsx
@@ -2,12 +2,12 @@ import './App.css'
 import Card from './components/card'
 
 function App() {
-     let src1 = 'https://images.pexels.com/photos/20582302/pexels-photo-20582302/free-photo-of-woman-eating-pizza.jpeg?auto=compress&cs=tinysrgb&w=600';
-     let src2 = 'https://images.pexels.com/photos/1598507/pexels-photo-1598507.jpeg?auto=compress&cs=tinysrgb&w=600'
+     let jacket = 'https://images.pexels.com/photos/20582302/pexels-photo-20582302/free-photo-of-woman-eating-pizza.jpeg?auto=compress&cs=tinysrgb&w=600';
+     let jeans = 'https://images.pexels.com/photos/1598507/pexels-photo-1598507.jpeg?auto=compress&cs=tinysrgb&w=600'
   return (
     <>
-      <Card productName='Utility Jacket' price='$110' src={src1} />
-      <Card productName='Jeans' price='$290' src={src2} />
+      <Card productName='Utility Jacket' price='$110' src={jacket} />
+      <Card productName='Jeans' price='$290' src={jeans} />
 
     </>
   )

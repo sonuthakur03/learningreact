9fadbcb29ed35f375840d172033ca11c4305a806
Sonu Thakur <rsonuth789@gmail.com>
2024-03-30 09:00:03 +0545

    removed console

--- 06CurrencyConverter/src/hooks/useCurrencyinfo.js
+++ 06CurrencyConverter/src/hooks/useCurrencyinfo.js
@@ -2,13 +2,10 @@ import {useEffect, useState} from "react"
 import apiKey from "../javascript/apikey"
 
 function useCurrencyInfo(currency, currencyTo){
-    console.log(currencyTo);
     const [data, setData] = useState({})
     useEffect(() => {
         fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/${currency}`).then((res) => res.json()) 
         .then((val) => {
-            console.log(val);
-            console.log(val.conversion_rates[currencyTo]);
             setData(val.conversion_rates[currencyTo])
         })
         

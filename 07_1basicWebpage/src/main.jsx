90a44c4f47cdbea7463ab8b936a723d08fe0002f
Sonu Thakur <rsonuth789@gmail.com>
2024-04-18 06:55:49 +0545

    finalizing project

--- 07_1basicWebpage/src/main.jsx
+++ 07_1basicWebpage/src/main.jsx
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client'
 import { Route ,RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import './index.css'
 import App from './App'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Services from './components/Services/Services'
+import Experiences from './components/Experiences/Experiences'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App/>} >
-
+      <Route path='' element={<Home/>} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact/>} />
+      <Route path='/services' element={<Services/>} />
+      <Route path='/experiences' element={<Experiences/>} />
     </Route>
   )
 )
